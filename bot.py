49e224f2c91b53fa793ff960f27c01bbb7708ffb
Nicklas Lindemann <9166512+Tiaryn@users.noreply.github.com>
2021-04-28 11:29:30 +0200

    added meeting start and stop

--- bot.py
+++ bot.py
@@ -1,60 +1,82 @@
 import discord
 import config
 
-class MyClient(discord.Client):
+intents = discord.Intents(members=True, messages=True, guilds=True, voice_states=True)
+client = discord.Client(intents=intents)
 
-	async def on_ready(self):
-		await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Christof Paar"))
-		print('Bot logged in as {0}'.format(self.user))
+# binding these vars to client limits the bot to one guild
+# no expansion is planned, so we work with this
+client.runningMeeting = False
+client.meetingMembers = set()
 
-	async def on_message(self, message):
-		# limit bot to designated bot channel
-		if message.channel.id in config.bot_channel_ids:
-			if message.content.startswith('$server'):
-				await message.channel.send('{0.guild.name} {0.guild.id}'.format(message))
+@client.event
+async def on_ready():
+	print('Bot logged in as {0}'.format(client.user))
 
-			if message.content.startswith('$roles'):
-				for r in message.guild.roles:
-					await message.channel.send('{0.name} {0.id}'.format(r).replace('@', ''))
+@client.event
+async def on_message(message):
+	# limit bot to designated bot channel
+	if message.channel.id in config.bot_channel_ids:
+		if message.content.startswith('$server'):
+			await message.channel.send('{0.guild.name} {0.guild.id}'.format(message))
 
-			if message.content.startswith('$vchannels'):
-				for v in message.guild.voice_channels:
-					await message.channel.send('{0.name} {0.id}'.format(v))
+		if message.content.startswith('$roles'):
+			for r in message.guild.roles:
+				await message.channel.send('{0.name} {0.id}'.format(r).replace('@', ''))
 
-			if message.content.startswith('$about'):
-				await message.channel.send('FSR-Bot with <3 from ~~TJ~~ FSR ETITS')
+		if message.content.startswith('$vchannels'):
+			for v in message.guild.voice_channels:
+				await message.channel.send('{0.name} {0.id}'.format(v))
 
+		if message.content.startswith('$about'):
+			await message.channel.send('FSR-Bot with <3 from ~~TJ~~ FSR ETITS')
 
-	async def on_voice_state_update(self, member, before, after):
-		
-		if after.channel != None:
-			if after.channel.id == config.channel_id:
+		if message.content.startswith('$start meeting'):
+			client.runningMeeting = True
+			await message.channel.send('Meeting started!')
 
-				guild = member.guild
-				role = guild.get_role(config.role_id)
+			# gather all already present members
+			channel = client.get_channel(config.channel_id)
+			
+			for member in channel.members:
+				client.meetingMembers.add(member.display_name)
 
-				if role is None:
-					return
+		if message.content.startswith('$stop meeting'):
+			client.runningMeeting = False
 
-				try:
-					await member.add_roles(role)
-				except Exception as e:
-					print(e)
+			# generate list of meeting participants and send message
+			mString = '** **\n'
+			for member in client.meetingMembers:
+				mString += member + '\n'
+			await message.channel.send(mString)
 
-		if before.channel != None:
-			if before.channel.id == config.channel_id:
-				
-				guild = member.guild
-				role = guild.get_role(config.role_id)
+			client.meetingMembers.clear()
 
-				if role is None:
-					return
+@client.event
+async def on_voice_state_update(member, before, after):
+	
+	# check if role needs to be added
+	if after.channel != None:
+		if after.channel.id == config.channel_id:
 
-				try:
-					await member.remove_roles(role)
-				except Exception as e:
-					print(e)
+			guild = member.guild
+			role = guild.get_role(config.role_id)
 
-client = MyClient()
+			# add role to member
+			await member.add_roles(role)
+
+			# log member
+			if client.runningMeeting:
+				client.meetingMembers.add(member.display_name)
+
+	# check if role needs to be removed
+	if before.channel != None:
+		if before.channel.id == config.channel_id:
+			
+			guild = member.guild
+			role = guild.get_role(config.role_id)
+
+			await member.remove_roles(role)
+
 client.run(config.token)
 
